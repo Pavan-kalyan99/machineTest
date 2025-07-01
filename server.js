9dd3a08a1a232340de8ff9677a7bfef64766f5da
lifehousebtmtech <saikrishnaarevarapu@gmail.com>
2025-07-01 14:36:19 +0530

    error fixed

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(distPath));
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     const indexPath = path.join(distPath, 'index.html');
     if (fs.existsSync(indexPath)) {
       res.sendFile(indexPath);
